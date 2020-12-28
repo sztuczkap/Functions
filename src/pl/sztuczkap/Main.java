82c0b3c27c95055a88a88f7f9c84d73cc91e1547
sztuczkap <p.sztuczka@wp.pl>
2020-12-28 21:46:41 +0100

    funkcje wyższych rzędów

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,31 @@
 package pl.sztuczkap;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        runnable("Helo").run();
+
+        List<String> names = new ArrayList<>();
+        names.add("Michał");
+        names.add("Adam");
+        names.add("Zygmunt");
+
+        Comparator<String> comparator = String::compareToIgnoreCase;
+        names.sort((reverse(comparator)));
+        names.forEach(System.out::println);
+
+    }
+
+    private static Runnable runnable(String text) {
+        return () -> System.out.println(text);
+    }
+
+    private static Comparator<String> reverse(Comparator<String> comp) {
+        return (x, y) -> comp.compare(y, x);
     }
 }
